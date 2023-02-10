34c49073d9c181c8910577fab866e1f3fcd7f289
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-02-10 22:10:19 +0300

    rename categories test to create category test
    Took 14 minutes

---
+++ tests/Feature/Api/V1/CreateCategoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\User;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
+
+test('user can create a category', function () {
+    $data = [
+        'name' => 'Main dishes',
+    ];
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->actingAs($this->user)->post(route('api.v1.categories.store'), $data);
+
+    $response->assertStatus(201);
+    $this->assertEquals(1, $this->user->categories()->count());
+    $this->assertEquals($data['name'], $this->user->categories()->first()->name);
+});
