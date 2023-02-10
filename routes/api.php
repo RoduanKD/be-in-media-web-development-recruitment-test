95b941f7e7e4dc96cb21bf4ee7428122166501cf
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-02-10 21:49:48 +0300

    install laravel actions
    Took 3 minutes
    
    Took 16 seconds

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Lorisleiva\Actions\Facades\Actions;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->name('api.v1.')->group(static function () {
+    Actions::registerRoutes();
 });
