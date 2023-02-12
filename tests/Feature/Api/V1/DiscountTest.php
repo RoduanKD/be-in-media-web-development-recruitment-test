039c68f2418be93037ef8bffd126a48788f10083
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-02-12 14:51:28 +0300

    test that user can NOT add discount to category he doesn't own
    Took 4 minutes

--- tests/Feature/Api/V1/DiscountTest.php
+++ tests/Feature/Api/V1/DiscountTest.php
@@ -32,3 +32,17 @@ test('user can add discount to a category', function () {
     $category->refresh();
     expect($category)->discount->toBe($data['discount']);
 });
+
+test('user can NOT add discount to a category he doesnt own', function () {
+    $category = Category::factory()->create(['user_id' => User::factory()->create()->id]);
+    $data = [
+        'discount' => 10,
+    ];
+
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $this->actingAs($this->user)->postJson(route('api.v1.category-discounts.update', $category), $data);
+
+    $response->assertStatus(403);
+    $category->refresh();
+    expect($category)->discount->toBe(0);
+});
